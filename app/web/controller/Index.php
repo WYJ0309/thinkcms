04274a822ac7d6597023b2e4d95b0c1b4a2394b9
wyj <3116373546@qq.com>
2019-12-08 16:44:46 +0800

    新闻两级 服务一级分类

--- app/web/controller/Index.php
+++ app/web/controller/Index.php
@@ -74,6 +74,9 @@ class Index extends Controller
         $data = request()->post();
         $is_zh = empty($data['is_zh'])?1:$data['is_zh'];//1中文 2英文
         $is_index = empty($data['is_index'])?0:1;//是否首页推荐
+        if(empty($data['cate_id'])){
+            return $this->result([],1,'请求成功,分类id不能为空','json');
+        }
         $where = [];
         if(!empty($is_index)){
             $where['top'] = 1;
@@ -83,9 +86,7 @@ class Index extends Controller
             $where['type'] = 1;//1文章分类 2服务分类
             $where['is_en'] = empty($is_zh)?1:2;//是否英文 1中文 2英文
         }
-        if(!empty($data['cate_id'])){
-            $where['pid'] = $data['cate_id'];
-        }
+        $where['pid'] = $data['cate_id'];
         $model = new Article();
         //是否英文 0中文 1英文
         $res = $model->getNewsList($where);
@@ -111,6 +112,9 @@ class Index extends Controller
         $data = request()->post();
         $is_zh = empty($data['is_zh'])?1:$data['is_zh'];//1中文 2英文
         $is_index = empty($data['is_index'])?0:1;//是否首页推荐
+        if(empty($data['cate_id'])){
+            return $this->result([],1,'请求成功,分类id不能为空','json');
+        }
         $where = [];
         if(!empty($is_index)){
             $where['top'] = 1;
@@ -120,6 +124,7 @@ class Index extends Controller
             $where['type'] = 2;//1文章分类 2服务分类
             $where['is_en'] = 1;//是否英文 1中文 2英文
         }
+        $where['pid'] = $data['cate_id'];
         $model = new Article();
         //是否英文 0中文 1英文
         $res = $model->getNewsList($where);
